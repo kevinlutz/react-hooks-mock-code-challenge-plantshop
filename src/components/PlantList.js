a3cae96dfbe08b1b320b1c06a598069a12cc98cb
+ <+>
2021-09-29 21:23:07 -0400

    new stuff

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+function PlantList({ plants }) {
+  const allPlants = plants.map((plantObj) => (
+    <PlantCard key={plantObj.id} {...plantObj} />
+  ));
+  return <ul className="cards">{allPlants}</ul>;
 }
 
 export default PlantList;
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((plantData) => setPlants(plantData));
+  }, []);
+
+  const addNewPlant = (newPlantObj) => {
+    setPlants((plantData) => [...plantData, newPlantObj]);
+  };
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant} />
+      <Search setSearch={setSearch} />
+      <PlantList
+        plants={plants.filter((plantObj) =>
+          plantObj.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+        )}
+      />
     </main>
   );
 }
